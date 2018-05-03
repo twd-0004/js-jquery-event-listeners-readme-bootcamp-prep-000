ab98cebd2d9ed350638caeb7403ee3ab0d3dffa3
Tyler Dreher <drehertyler@gmail.com>
2018-05-03 16:54:54 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -17,7 +17,8 @@ function pressIt() {
   })
 }
 function submitIt() {
-  $('form').on('submit', alert('Your form is going to be submitted now.'))
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.'))
 }
 $(document).ready(function(){
 // call functions here
