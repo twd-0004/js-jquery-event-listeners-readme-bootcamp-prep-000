a45dd349c6e906e9c7fd70ef2527b34dbf7e4b6c
Tyler Dreher <drehertyler@gmail.com>
2018-05-03 16:49:30 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -7,7 +7,7 @@ function frameIt() {
 }
 function pressIt() {
   $(':input').on('keydown', function(e) {
-    console.log(e)
+    console.log(e.keyCode)
 
     if (parseInt(e) === 71)
       alert('You pressed G!')
