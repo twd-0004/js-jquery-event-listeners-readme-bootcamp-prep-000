c028e9d613c4d30a2f9907b359652d8bc0c89229
Tyler Dreher <drehertyler@gmail.com>
2018-05-03 16:31:49 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,5 +1,10 @@
 //define functions here
-
+function getIt() {
+  $('p').on('click', alert('Hey!'));
+}
+function frameIt() {
+  $('img').on('load', this.addClass('tasty'))
+}
 $(document).ready(function(){
 
 // call functions here
