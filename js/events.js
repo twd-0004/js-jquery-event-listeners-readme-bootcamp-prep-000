5038e0b55203af850bfe04716e0c2184c7855793
Tyler Dreher <drehertyler@gmail.com>
2018-05-03 16:40:09 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -5,8 +5,19 @@ function getIt() {
 function frameIt() {
   $('img').on('load', $(this).addClass('tasty'))
 }
+function pressIt() {
+  $(':input').on('keydown', function(e) {
+    if (e === 71)
+      alert('You pressed G!')
+  })
+}
+function submitIt() {
+  $('form').on('submit', alert('Your form is going to be submitted now.'))
+}
 $(document).ready(function(){
-
 // call functions here
-
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
